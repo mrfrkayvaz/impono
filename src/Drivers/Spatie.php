287bd2bfd49724c56d73d02539f326f873bdd89a
Ömer Faruk Ayvaz <mrfrkayvaz@gmail.com>
2026-01-18 19:38:16 +0300

    fix: temp file image load bug fixed

--- src/Drivers/Spatie.php
+++ src/Drivers/Spatie.php
@@ -14,7 +14,7 @@ class Spatie {
 
     public function __construct(FileData $fileData) {
         $this->fileData = $fileData;
-        $path = Storage::disk($this->fileData->getDisk())->path($this->fileData->getURL());
+        $path = Storage::disk("local")->path($this->fileData->getURL());
         $this->image = Image::load($path);
     }
 
