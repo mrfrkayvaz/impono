8bcd361e746be6fa59e5961f404a3bd709e85bfc
Ömer Faruk Ayvaz <mrfrkayvaz@gmail.com>
2026-01-19 18:24:25 +0300

    tests updated

--- tests/Unit/FileServiceTest.php
+++ tests/Unit/FileServiceTest.php
@@ -19,19 +19,3 @@ it('sends file to storage', function () {
         ->and($file_data->getIsTemp())
         ->toBeFalse();
 });
-
-it('sends file to default location', function () {
-    $file = getAssetFile();
-
-    config(['filesystem.disks.local.root' => 'images']);
-
-    $source = new ImponoFileSource($file);
-    $file_data = Impono::load($source)
-        ->disk('local')
-        ->push('elephant.png');
-
-    expect($file_data->getPath())
-        ->toBe('images/elephant.png')
-        ->and($file_data->getIsTemp())
-        ->toBeFalse();
-});
